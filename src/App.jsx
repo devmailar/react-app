6c429220ee0b7519de25fdffd13e7a4a0118129f
Mailar <veex1233@gmail.com>
2021-12-17 23:42:06 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import cat from './assets/cat.png';
+
 import './App.css';
 
 function App() {
@@ -6,17 +8,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={cat} className="Cat-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello and welcome to my website.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
