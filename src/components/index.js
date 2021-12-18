aa0aab8ae2b9437b1e98e7231f64aa35a43787e8
Mailar <veex1233@gmail.com>
2021-12-18 16:23:18 +0200

    Update

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-export { default as Navigation } from "./Navigation";
-export { default as Footer } from "./Footer";
+export { default as Navigation } from "./Navbar/Navigation";
+export { default as Footer } from "./Footer/Footer";
 export { default as Home } from "./Home/Home";
-export { default as About } from "./About";
-export { default as Contact } from "./Contact";
-export { default as Blog } from "./Blog";
+export { default as About } from "./About/About";
+export { default as Contact } from "./Contact/Contact";
+export { default as Blog } from "./Blog/Blog";
