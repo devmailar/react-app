e1df25670704c99a847796c2b264075b51dcd61c
mailar <veex1233@gmail.com>
2022-05-11 15:22:01 +0300

    Test

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ExpenseItem from '../ExpenseItem/ExpenseItem';
+import './Expenses.css';
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+        currency={props.items[0].currency}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+        currency={props.items[1].currency}
+      />
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+        currency={props.items[2].currency}
+      />
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+        currency={props.items[3].currency}
+      />
+    </div>
+  );
+}
+
+export default Expenses;
