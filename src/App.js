23f5e25b04b65466eef13a9b035353dc1a8effad
mailar <veex1233@gmail.com>
2022-06-01 22:54:29 +0300

    Update core component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
       title: 'Toilet Paper',
-      amount: 94.12,
+      amount: 940.0,
       date: new Date(2022, 7, 14),
       currency: 'EUR',
     },
     {
       id: 'e2',
       title: 'New TV',
-      amount: 799.49,
+      amount: 799.0,
       date: new Date(2022, 2, 12),
       currency: 'EUR',
     },
     {
       id: 'e3',
       title: 'Car Insurance',
-      amount: 294.67,
+      amount: 294.0,
       date: new Date(2022, 2, 28),
       currency: 'EUR',
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
-      amount: 450,
+      amount: 195.0,
       date: new Date(2022, 5, 12),
       currency: 'EUR',
     },
@@ -35,7 +35,6 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started</h2>
       <Expenses items={expenses} />
     </div>
   );
--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -2,18 +2,21 @@ import React from 'react';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import Card from '../Card/Card';
 import './ExpenseItem.css';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 
+const ExpenseItem = (props) => {
 
-function ExpenseItem(props) {
+  const Log = () => alert('clicked')
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>
-          {props.amount} {props.currency}
-        </div>
       </div>
+      <Button onClick={Log} variant="contained" className="ei-blue" endIcon={<ShoppingBagIcon />}>BUY</Button>
     </Card>
   );
 }
